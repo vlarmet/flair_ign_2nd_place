90cf9a8774bd655ae5d0e622889190f458fa5cf4
vlarmet <larmet.vincent@@gmail.com>
2023-03-24 21:02:15 +0100

    initialization

---
+++ utils/train.py
@@ -0,0 +1,119 @@
+import numpy as np
+import tensorflow as tf
+from osgeo import gdal
+
+def read_image(image_path, mask=False):
+    
+    im = gdal.Open(image_path)
+    if mask:
+        image = im.ReadAsArray().transpose()
+        image = np.where(np.isin(image, [19,13,14,15,16,17,18]), 13, image) - 1
+
+        #image = to_categorical(image).astype(np.float32)
+        #image = np.expand_dims(image, axis = 2)
+    else:
+        image = im.ReadAsArray().transpose().astype(np.float32)[:,:,[0,1,2]]      # rgb
+        #image = image / 127.5 - 1
+        #image = image / 255.0
+        '''
+        for i, (avg, std) in enumerate(zip(np.array([0.485, 0.456, 0.406]), np.array([0.229, 0.224, 0.225]))):
+          image[:,:,i] = ((image[:,:,i]/255.0) - avg)/std
+        '''
+
+
+    im = None
+
+    return image
+
+
+
+class Datagen(tf.keras.utils.Sequence):
+    def __init__(self, path_list, batch_size, random_state, val_rate, train, return_x_only = False, resize_label = True, resize_x = True, augment = None):
+
+        self.batch_size = batch_size
+        self.random_state = random_state
+
+        self.ids = np.array(path_list)
+        self.train= train
+
+        self.rng = np.random.RandomState(random_state)
+        self.rng.shuffle(self.ids)
+        if train:
+            self.ids = self.ids[:round((1 - val_rate) * len(self.ids))]
+        else :
+            self.ids = self.ids[round((1 - val_rate) * len(self.ids)):]
+        self.current_index = 0
+        self.num_batch = 0
+        self.return_x_only = return_x_only
+        self.resize_label = resize_label
+        self.resize_x = resize_x
+        self.augment = augment
+
+    def __augment(self, x, y):
+        aug = self.augment(image = x, mask = y)
+
+        return aug["image"], aug["mask"]
+
+    def __len__(self):
+        ''' return total number of batches '''
+        return math.floor(len(self.ids)/self.batch_size)
+
+    def on_epoch_end(self):
+        self.current_index = 0
+        self.num_batch = 0
+        if self.train : self.rng.shuffle(self.ids)
+        ''' shuffle data after every epoch '''
+        # fix on epoch end it's not working, adding shuffle in len for alternative
+        pass
+
+    def __getitem__(self, idx):
+        
+        if self.num_batch == self.__len__() - 1 or self.current_index > len(self.ids) - self.batch_size:
+            self.current_index = 0
+            self.num_batch = 0
+            if self.train : self.rng.shuffle(self.ids)
+
+        
+
+        # list of current batch indexes
+        batch_ids = self.ids[self.current_index:(self.current_index + self.batch_size)]
+
+        x = []
+        y = []
+        
+        for img_path, msk_path in batch_ids:
+            x.append(read_image(img_path, mask=False, resize=self.resize_x))
+            y.append(read_image(msk_path, mask=True, resize = self.resize_label))
+
+
+        if len(x) == 0:
+            print(self.current_index)
+
+            for img_path, msk_path in self.ids[0:(0 + self.batch_size)]:
+                x.append(read_image(img_path, mask=False, resize=self.resize_x))
+                y.append(read_image(msk_path, mask=True, resize = self.resize_label))
+
+
+        x = np.concatenate([np.expand_dims(img, axis=0) for img in x], axis=0)#[:,:3,:,:] # rgb
+        y = np.concatenate([np.expand_dims(msk, axis=0) for msk in y], axis=0)
+        
+        if self.augment is not None:
+            for i in range(x.shape[0]):
+                new_x, new_y = self.__augment(x[i,:,:,:].astype(np.uint8), y[i,:,:])
+                x[i,:,:,:] = new_x
+                y[i,:,:] = new_y
+
+       
+        x = x.astype(np.float32)       
+
+        self.current_index += self.batch_size
+        self.num_batch += 1
+        
+        # augmentation
+        #x, y = self.__data_augmentation(x, y)
+
+
+        if self.return_x_only:
+            return tf.convert_to_tensor(x)
+        else:
+            return tf.convert_to_tensor(x), tf.convert_to_tensor(y)
